9a67bcf1acb2c34ef0dab13cbb136b5cf7c04322
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-03-07 23:03:34 +0100

    Changes after code review

--- src/App.js
+++ src/App.js
@@ -2,30 +2,68 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import AddItem from "./components/AddItem";
-import ListItem from "./components/ListItem";
+import List from "./components/List";
 import { nanoid } from "nanoid";
 import SearchItem from "./components/SearchItem";
+import { saveToLocal, loadFromLocal } from "./lib/localStorage";
+import SearchBar from "./components/Searchbar";
 
 function App() {
-  const [shoppingList, setShoppingList] = useState([]);
-  // const [apiURL, setApiURL] = useState(
-  //   "https://fetch-me.vercel.app/api/shopping/items"
-  // );
+  const [shoppingList, setShoppingList] = useState(
+    loadFromLocal("items") ?? []
+  );
+  const [searchTerm, setSearchTerm] = useState("");
+  const [shoppingItems, setShoppingItems] = useState([]);
 
   useEffect(() => {
-    loadItems();
-    async function loadItems() {
+    loadShoppingItems();
+    async function loadShoppingItems() {
       try {
         const response = await fetch(
           "https://fetch-me.vercel.app/api/shopping/items"
         );
         const data = await response.json();
-        setShoppingList(data.data);
+        setShoppingItems(data.data);
       } catch (error) {
         console.log(error);
       }
     }
   }, []);
+
+  useEffect(() => {
+    saveToLocal("items", shoppingList);
+  }, [shoppingList]);
+
+  return (
+    <div className="App">
+      <Header />
+
+      <AddItem onAddItem={handleAddNewItem} />
+      <SearchBar onSearch={handleSearch} searchTerm={searchTerm} />
+      {searchTerm && (
+        <SearchItem
+          searchTerm={searchTerm}
+          shoppingItems={shoppingItems}
+          onhandleFilteredItem={handleFilteredItem}
+        />
+      )}
+      <List shoppingList={shoppingList} onDeleteItem={handleDeleteItem} />
+    </div>
+  );
+
+  function handleFilteredItem(shoppingItem) {
+    if (shoppingList.find((item) => item._id === shoppingItem._id)) {
+      alert("You already added the item");
+      setSearchTerm("");
+    } else {
+      setSearchTerm("");
+      setShoppingList([...shoppingList, shoppingItem]);
+    }
+  }
+
+  function handleSearch(title) {
+    setSearchTerm(title);
+  }
 
   function handleDeleteItem(itemID) {
     setShoppingList(shoppingList.filter((item) => item._id !== itemID));
@@ -42,15 +80,6 @@ function App() {
       },
     ]);
   }
-
-  return (
-    <main className="App">
-      <Header />
-      <AddItem onAddItem={handleAddNewItem} />
-      <SearchItem listItems={shoppingList} />
-      <ListItem listItems={shoppingList} onDeleteItem={handleDeleteItem} />
-    </main>
-  );
 }
 
 export default App;
--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,41 +1,24 @@
-import "./AddItem.css";
-import { useState } from "react";
-
-function SearchItem({ onSearchItem, listItems }) {
-  const [searchItem, setSearchItem] = useState("");
+export default function SearchItem({
+  searchTerm,
+  shoppingItems,
+  onhandleFilteredItem,
+}) {
+  const filteredItems = shoppingItems.filter((item) =>
+    item.name.en.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
-    <>
-      <label htmlFor="search">What do you want to buy?</label>
-      <div className="SearchBar">
-        <input
-          id="search"
-          type="text"
-          className="Input"
-          placeholder="Search items"
-          onChange={(event) => setSearchItem(event.target.value)}
-        />
-        <ul className="List">
-          {listItems
-            .filter((item) => {
-              if (searchItem === "") {
-                return "";
-              } else if (
-                item.name.en.toLowerCase().includes(searchItem.toLowerCase())
-              ) {
-                return item;
-              }
-            })
-            .map((item) => {
-              return (
-                <li key={item._id} item={item}>
-                  <button className="Button">{item.name.en}</button>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-    </>
+    <ul className="Search-List">
+      {filteredItems.map((item) => (
+        <li
+          className="Search-List-Item"
+          key={item._id}
+          onClick={() => {
+            onhandleFilteredItem(item);
+          }}
+        >
+          {item.name.en}
+        </li>
+      ))}
+    </ul>
   );
 }
-
-export default SearchItem;
